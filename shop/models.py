e3cbd888bfe4dcc4e958fb20ffb37b47b867d013
Reza Mahmoudian <86279610+rezamahmoudian@users.noreply.github.com>
2022-08-24 09:46:11 +0430

    add category and product models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Categoty(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.CharField(max_length=200, db_index=True)
+
+    class Mete:
+        ordering = ["name"]
+        verbose_name = "category "
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey("Categoty", related_name="products", on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.CharField(max_length=200, db_index=True)
+    #
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["name"]
+        #
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
