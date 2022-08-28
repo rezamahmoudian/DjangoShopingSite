4c7ec75a1bd3c07fa8a25e78fef7eb51b8fa3f68
Reza Mahmoudian <86279610+rezamahmoudian@users.noreply.github.com>
2022-08-28 11:40:26 +0430

    create model admins for category and product models

--- shop/models.py
+++ shop/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-class Categoty(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
     slug = models.CharField(max_length=200, db_index=True)
 
