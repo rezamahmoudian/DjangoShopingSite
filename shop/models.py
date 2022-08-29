b77693dd157511e9ad81eb13cd46efacc0be7f23
Reza Mahmoudian <86279610+rezamahmoudian@users.noreply.github.com>
2022-08-29 12:21:53 +0430

    add get absolute url to models

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from django.urls import reverse
 
 # Create your models here.
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200, db_index=True)
@@ -14,6 +15,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("shop:product_list_category", args=[self.slug])
 
 
 class Product(models.Model):
@@ -35,3 +39,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("shop:product_detail", args=[self.id, self.slug])
