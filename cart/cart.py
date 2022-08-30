a94f4a36060748913e98da84f6cd84793a7b8912
Reza Mahmoudian <86279610+rezamahmoudian@users.noreply.github.com>
2022-08-30 12:55:45 +0430

    create cart.py in cart app + create cart class + create add prouduct to cart method

--- cart/cart.py
+++ cart/cart.py
@@ -3,6 +3,7 @@ from shop.models import Product
 
 
 class Cart:
+
     def __init__(self, request):
         self.session = request.session
         cart = self.session.get(settings.CART_SESSION_ID)
@@ -10,4 +11,16 @@ class Cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
 
-        
+    # add to cart
+    def add(self, product, quantity):
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0, 'price': product.price}
+        self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        self.session.modified = True
+
+    # remove from cart
+    
