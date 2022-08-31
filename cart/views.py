f0a7f5332181a061c6ed402c193535f9c3e8f0a1
Reza Mahmoudian <86279610+rezamahmoudian@users.noreply.github.com>
2022-08-31 12:59:26 +0430

    add def cart_remove and cart_detail to views

--- cart/views.py
+++ cart/views.py
@@ -15,4 +15,17 @@ def cart_add(request, product_id):
     if form.is_valid():
         cd = form.cleaned_data
         cart.add(product=product, quantity=cd['quantity'])
-    return redirect('cart:cart_details')
+    return redirect('cart:cart_detail')
+
+
+def cart_remove(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+
+def cart_remove(request):
+    cart = Cart(request)
+
+    return render(request, 'cart/detail.html', {'cart': cart})
