a71ee8d980526feae608d4f9db3403f18cf3f212
Reza Mahmoudian <86279610+rezamahmoudian@users.noreply.github.com>
2022-08-31 11:32:39 +0430

    add cart_add view

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import CartAddProductForm
+from .cart import Cart
+from shop.models import Product
+from django.views.decorators.http import require_POST
+
 
 # Create your views here.
+
+@require_POST
+def cart_add(request, product_id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'])
+    return redirect('cart:cart_details')
