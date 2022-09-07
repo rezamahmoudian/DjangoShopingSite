3eef13bed9768ef1acf054c22705607f4cdaa1f0
Reza Mahmoudian <86279610+rezamahmoudian@users.noreply.github.com>
2022-09-07 15:32:33 +0430

    add order created form and order create view

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .forms import OrderCreateForm
+from .models import Order
+from .models import OrderItem
+from cart.cart import Cart
 
 # Create your views here.
+
+
+def order_crate(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm()
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order, product=item['product'], quantity=item['quantity'], price=item['price'])
+            return render(request, 'templates/orders/order/create.html', {'order': order})
+        else:
+            form = OrderCreateForm()
+        return render(request, 'templates/orders/order/create.html', {'form': form, 'cart': cart})
+
+
+
+
