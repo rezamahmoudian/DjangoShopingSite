ea57dd927c3dd040864d8abf838429e55a25a453
Reza Mahmoudian <86279610+rezamahmoudian@users.noreply.github.com>
2022-09-25 23:00:05 +0330

    add export csv to order admin actions

--- orders/admin.py
+++ orders/admin.py
@@ -43,3 +43,4 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_csv]
