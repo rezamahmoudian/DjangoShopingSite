d059a438d890ab1418ed8e4ec5c50b00b5d363d9
Reza Mahmoudian <86279610+rezamahmoudian@users.noreply.github.com>
2022-10-26 23:20:11 +0330

    fix a bug in coupon apply view

--- coupons/views.py
+++ coupons/views.py
@@ -4,13 +4,16 @@ from .models import Coupon
 from .forms import CouponApplyForm
 from django.views.decorators.http import require_POST
 
+
 # Create your views here.
 
 
 @require_POST
 def coupon_apply_view(request):
     now = timezone.now()
-    code = CouponApplyForm.cleaned_data['code']
+    coupon_form = CouponApplyForm(request.POST)
+    if coupon_form.is_valid():
+        code = coupon_form.cleaned_data['code']
     try:
         coupon = Coupon.objects.get(code__iexact=code, valid_from__lte=now, valid_to__gte=now)
         request.session['coupon_id'] = coupon.id
