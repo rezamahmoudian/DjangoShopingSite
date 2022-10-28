89652e399ad53c69f433de289cb57a1e491a14fa
Reza Mahmoudian <86279610+rezamahmoudian@users.noreply.github.com>
2022-10-28 16:42:27 +0330

    first makemessages and compile messages

--- djangoShopingSite/settings.py
+++ djangoShopingSite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,8 +142,8 @@ os.environ['PATH'] = GTK_FOLDER + os.pathsep + os.environ.get('PATH', '')
 
 # languages
 LANGUAGES = (
-    ('en', 'English'),
-    ('fa', 'Farsi')
+    ('en', _('English')),
+    ('fa', _('Farsi'))
 )
 
 # locale dir
